52a75471bfc63295812bc3855f779488d4babfe6
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-07-30 13:18:51 +0200

    add pseudo-code to get better understanding of classification

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("start");
+console.log("Welcome aboard fren !");
+
+console.log("let's shuffle the cards..");
+
+{
+  /*
+Chaque carte est a classé entre une valeur de 2 a 14
+Value = 
+2=2; 3=3; 4=4; 5=5; 6=6; 7=7; 8=8; 9=9; 10=10; J=11; Q=12; K=13; A=14
+Chaque carte peut etre à la fois Club (♣), Diamond (♦), Heart (♥) and Spade (♠).
+
+
+
+*/
+}

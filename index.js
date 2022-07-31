cc6fb06a47a71df00a883c55929233a00e34fca0
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-07-31 10:50:26 +0200

    add a function to dispatch cards between player, bank and table

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-import { generateDeck, getNineCardsFromADeck } from "./lib/generateDeck.js";
-import { suits, values, isSameSuits } from "./lib/cards.js";
+import { suits, values } from "./lib/cards.js";
+import {
+  generateDeck,
+  getNineCardsFromADeck,
+  dispatchCardsBetweenPlayersAndTable,
+} from "./lib/deck.js";
 
 console.log("Welcome aboard fren !");
 console.log("let's shuffle the cards..");
@@ -9,3 +13,8 @@ const nineRandomCards = getNineCardsFromADeck(newDeck);
 
 console.log("...aaaaaaaannd let's see !");
 console.log(nineRandomCards);
+
+const displatchedCards = dispatchCardsBetweenPlayersAndTable(nineRandomCards);
+console.log("Your cards : ", displatchedCards.playerDeck);
+console.log("Bank cards : ", displatchedCards.bankDeck);
+console.log("Table cards : ", displatchedCards.commonDeck);

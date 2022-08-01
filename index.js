667ea61f7789fe72df1d6bca92c28f196603b340
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-08-01 17:14:41 +0200

    refacto code and add way to categorize card between values and suits

--- index.js
+++ index.js
@@ -1,75 +1,32 @@
-import inquirer from "inquirer";
-import { suits, values, dispatchCardsBetweenPlayers } from "./lib/cards.js";
+//import inquirer from "inquirer";
+import { suits, values, distributeCards } from "./lib/cards.js";
 import {
   generateDeck,
+  shuffleDeck,
   getTenCardsFromADeck,
+  improveCardsReadability,
   hideBankDeck,
-  sortDeckBy,
+  sortDeckByValuesAndSuits,
 } from "./lib/deck.js";
 
+let amountBet = 1;
 console.log("Welcome aboard fren !");
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "betAmount",
-      message: "How much ETH you want Bet?",
-      choices: [
-        { name: "0.01", value: 0.01 },
-        { name: "0.1", value: 0.1 },
-        { name: "1", value: 1 },
-      ],
-    },
-  ])
-  .then((answers) => {
-    console.info("You choose to bet:", answers.betAmount, "ETH");
-    console.log("Now I'm going to shuffle the cards and deal them !");
-    console.log("May the force be with you...");
-    const newDeck = generateDeck(suits, values);
-    const TenRandomCards = getTenCardsFromADeck(newDeck);
-    const displatchedCards = dispatchCardsBetweenPlayers(TenRandomCards);
-    console.log("Your cards : ", displatchedCards.playerDeck);
-    console.log("Bank cards : ", hideBankDeck(displatchedCards.bankDeck));
-    console.log("Do you want to continue or give up ?");
-    inquirer
-      .prompt([
-        {
-          type: "list",
-          name: "choiceAfterGetCards",
-          message: "If you want to continue you must double your initial bet",
-          choices: [
-            {
-              name: `continue and bet ${2 * answers.betAmount}ETH in more`,
-              value: 2 * answers.betAmount,
-            },
-            { name: "Give up !", value: 0 },
-          ],
-        },
-      ])
-      .then((answers) => {
-        let amountBet = answers.choiceAfterGetCards;
-        let initialBet = amountBet / 2;
-        if (amountBet > 0) {
-          console.log(
-            `You choose to continue and add ${amountBet} ETH, to your initial bet of ${initialBet} ETH`
-          );
-          console.log("the dealer turns over the bank's cards...");
-          console.log(displatchedCards.bankDeck);
-          console.log(
-            "bank deck sorted by value : ",
-            sortDeckBy(displatchedCards.bankDeck, "value")
-          );
-          console.log(
-            "bank deck sorted by suits : ",
-            sortDeckBy(displatchedCards.bankDeck, "suits")
-          );
-
-          // Determiner Si banque est qualifié (As + K ou plus..)
-
-          // Determiner quel est la meilleur combinaison dans un deck de 5 carte
-          // Sort cards
-
-          // Determiner qui gagne entre user et banque
-        }
-      });
-  });
+console.log(`You choose to bet ${amountBet}ETH`);
+console.log("Now I'm going to shuffle the cards and deal them !");
+console.log("May the force be with you...");
+const newDeck = generateDeck(suits, values);
+const deckShuffled = shuffleDeck(newDeck);
+const tenCards = getTenCardsFromADeck(deckShuffled);
+const distributedCards = distributeCards(tenCards);
+const { playerDeck, bankDeck } = distributedCards;
+const bankDeckReadable = improveCardsReadability(bankDeck, values, suits);
+const playerDeckReadable = improveCardsReadability(playerDeck, values, suits);
+const hiddenBankDeck = hideBankDeck(bankDeckReadable);
+console.log("Bank deck :", hiddenBankDeck);
+console.log("Player deck :", playerDeckReadable);
+console.log(`You choose to bet moar to see the bank cards...`);
+console.log("Bank deck :", bankDeckReadable);
+console.log(`Analyse of PlayerDeck...`);
+const { sortedValues, sortedSuits } = sortDeckByValuesAndSuits(playerDeck);
+console.log("player deck sorted by values :", sortedValues);
+console.log("player deck sorted by suits :", sortedSuits);

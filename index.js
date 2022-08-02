32b7010868ec4d0c08cfa943de360198ca6d3486
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-08-02 11:08:24 +0200

    make some refacto and add functions to resolve hands and rank it by values

--- index.js
+++ index.js
@@ -8,22 +8,29 @@ import {
   hideBankDeck,
   sortDeckByValuesAndSuits,
 } from "./lib/deck.js";
-import { isFlush, isStraight, getDuplicateValues } from "./lib/ranking.js";
+import { getRankingOfDeck } from "./lib/ranking.js";
 
+console.log("Hey Fren, Take seat and bet 1ETH");
 const newDeck = generateDeck(suits, values);
 const deckShuffled = shuffleDeck(newDeck);
 const tenCards = getTenCardsFromADeck(deckShuffled);
 const distributedCards = distributeCards(tenCards);
 const { playerDeck, bankDeck } = distributedCards;
+// const playerDeck = ["DC", "EC", "FH", "GH", "HD"] =====> exemple of a given player deck
+// const bankDeck = ["LS", "JC", "AD", "KH", "MS"] =====> exemple of a given bank deck
+
 const bankDeckReadable = improveCardsReadability(bankDeck, values, suits);
 const playerDeckReadable = improveCardsReadability(playerDeck, values, suits);
 const hiddenBankDeck = hideBankDeck(bankDeckReadable);
 console.log("Bank deck :", hiddenBankDeck);
 console.log("Player deck :", playerDeckReadable);
 console.log("Bank deck :", bankDeckReadable);
-const { sortedValues, sortedSuits } = sortDeckByValuesAndSuits(playerDeck);
-console.log("player deck sorted by values :", sortedValues);
-console.log("player deck sorted by suits :", sortedSuits);
-console.log("is Flush ? : ", isFlush(sortedSuits));
-console.log("is Straight ? : ", isStraight(sortedValues));
-console.log("get duplicates values  : ", getDuplicateValues(sortedValues));
+const playerDeckSorted = sortDeckByValuesAndSuits(playerDeck);
+const bankDeckSorted = sortDeckByValuesAndSuits(bankDeck);
+
+console.log("ranking of player deck:", getRankingOfDeck(playerDeckSorted));
+console.log("ranking of bank deck:", getRankingOfDeck(bankDeckSorted));
+
+//handle case of Royal Flush
+//handle case of AK
+// Handle case of 'Who win' between same result; (better ranking)

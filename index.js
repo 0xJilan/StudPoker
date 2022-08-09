9c5a73f715564180fed6c9365f72421b6500993b
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-08-09 14:18:04 +0200

    finish algorithme and add a way to control the number of result

--- index.js
+++ index.js
@@ -8,25 +8,48 @@ import {
   hideBankDeck,
   sortDeckByValuesAndSuits,
 } from "./lib/deck.js";
-import { getRankingOfDeck } from "./lib/ranking.js";
+import {
+  getRankingOfDeck,
+  getWinner,
+  isBankQualified,
+  getPayout,
+} from "./lib/ranking.js";
 
-console.log("Hey Fren, Take seat and bet 1ETH");
-const newDeck = generateDeck(suits, values);
-const deckShuffled = shuffleDeck(newDeck);
-const tenCards = getTenCardsFromADeck(deckShuffled);
-const distributedCards = distributeCards(tenCards);
-const { playerDeck, bankDeck } = distributedCards;
-const bankDeckReadable = improveCardsReadability(bankDeck, values, suits);
-const playerDeckReadable = improveCardsReadability(playerDeck, values, suits);
-const hiddenBankDeck = hideBankDeck(bankDeckReadable);
-console.log("Bank deck :", hiddenBankDeck);
-console.log("Player deck :", playerDeckReadable);
-console.log("Bank deck :", bankDeckReadable);
-const playerDeckSorted = sortDeckByValuesAndSuits(playerDeck);
-const bankDeckSorted = sortDeckByValuesAndSuits(bankDeck);
-console.log("ranking of player deck:", getRankingOfDeck(playerDeckSorted));
-console.log("ranking of bank deck:", getRankingOfDeck(bankDeckSorted));
+let amountBet = 0.1;
+let BankReserve = 100;
+let PlayerReserve = 100;
+let partyToDo = 1000;
 
-// TODO: Handle case of 'Who win' between same result; (better ranking)
-// TODO: Handle comparaison of two games
+const play = () => {
+  console.log(`Hey Fren, Take seat and bet ${amountBet}ETH`);
+  const newDeck = generateDeck(suits, values);
+  const deckShuffled = shuffleDeck(newDeck);
+  const tenCards = getTenCardsFromADeck(deckShuffled);
+  const distributedCards = distributeCards(tenCards);
+  const { playerDeck, bankDeck } = distributedCards;
+  const bankDeckReadable = improveCardsReadability(bankDeck, values, suits);
+  const playerDeckReadable = improveCardsReadability(playerDeck, values, suits);
+  const hiddenBankDeck = hideBankDeck(bankDeckReadable);
+  //console.log("Bank deck :", hiddenBankDeck);
+  const playerDeckSorted = sortDeckByValuesAndSuits(playerDeck);
+  const bankDeckSorted = sortDeckByValuesAndSuits(bankDeck);
+  const playerResult = getRankingOfDeck(playerDeckSorted);
+  console.log("Player deck :", playerDeckReadable);
+  const bankResult = getRankingOfDeck(bankDeckSorted);
+  console.log("Bank deck :", bankDeckReadable);
+  const winner = getWinner(bankResult, playerResult);
+  const qualified = isBankQualified(bankResult);
+  const payout = getPayout(winner, qualified, playerResult, amountBet);
+  console.log(payout);
+  BankReserve += payout.bankPayout;
+  PlayerReserve += payout.playerPayout;
+};
+let n = 0;
+while (n < partyToDo) {
+  play();
+  n++;
+}
+console.log("################## FINAL RESULT !!!!!!! #############");
+console.log({ BankReserve: BankReserve, PlayerReserve: PlayerReserve });
+
 // TODO: Add a 500 party simulator to know who wins between bank and player
